58eeea3da549f5b84a9d6dae682c533610eca932
Karthik Sunil K <karthiksunilk@Karthiks-MacBook-Air.local>
2025-07-27 15:14:44 +0530

    1201 is added

---
+++ starter.sh
@@ -0,0 +1,18 @@
+FOLDER_NAME="1201"
+
+mkdir "$FOLDER_NAME" && cat > "$FOLDER_NAME/README.md" <<EOF
+# $FOLDER_NAME
+
+## Code
+
+\`\`\`python
+# Paste your code here
+\`\`\`
+
+## Logic
+
+- Step 1: 
+- Step 2: 
+- Step 3: 
+
+EOF
